95c8507350ce6d6a363e00b699f85c79b8ffc5cc
genekellyboyle <DrDuffy@bhhsnv.com>
2025-10-03 17:49:39 -0700

    Remove debug logging from Cloudinary configuration

--- utils/cloudinary.ts
+++ utils/cloudinary.ts
@@ -1,12 +1,5 @@
 import cloudinary from "cloudinary";
 
-// Debug environment variables
-console.log('Cloudinary Environment Variables:');
-console.log('NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME:', process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME);
-console.log('CLOUDINARY_API_KEY:', process.env.CLOUDINARY_API_KEY ? 'SET' : 'NOT SET');
-console.log('CLOUDINARY_API_SECRET:', process.env.CLOUDINARY_API_SECRET ? 'SET' : 'NOT SET');
-console.log('CLOUDINARY_FOLDER:', process.env.CLOUDINARY_FOLDER);
-
 cloudinary.v2.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
